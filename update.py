f9b09e621f0ee7ebf303e251b4c3e66a31f5b802
ricardoRodiani <ricardo.rodiani@gmail.com>
2019-11-25 22:59:17 -0300

    ricardo
    chora

--- update.py
+++ update.py
@@ -5,7 +5,7 @@ bd = Conexao('educatorrdb.ccqy8084hset.sa-east-1.rds.amazonaws.com','postgres','
     
 def inserir(tupla, table):
     if table == 1:
-        teste = bd.manipular("INSERT INTO professor VALUES ('777', 'Samuel', '1');")
+        teste = bd.manipular("INSERT INTO professor VALUES ('"+str(tupla[0])+"', '"+str(tupla[1])+"', '"+str(tupla[2])+"');")
         print(teste)
 
 def deletar(tupla, table):
