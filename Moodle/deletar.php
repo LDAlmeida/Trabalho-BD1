c5409fa26c6ad5a70b5f55dfac40138d52f28426
Samuel Souza <48895781+samuelsouza001@users.noreply.github.com>
2019-11-28 17:26:39 -0300

    .

--- Moodle/deletar.php
+++ Moodle/deletar.php
@@ -1,12 +1,46 @@
 <?php
 include_once("conexao.php");
 
-function professor(){
-$nomep = filter_input(INPUT_POST, 'cpfp',FILTER_SANITIZE_STRING);
-$cpfp = filter_input(INPUT_POST, 'nomep',FILTER_SANITIZE_EMAIL);
-$depp = filter_input(INPUT_POST, 'depp',FILTER_SANITIZE_STRING);
 
-$saida ="INSERT INTO professor(cpfp,nomep,depp) VALUES ($cpf,$nome,$dnúmero)";
+function departamento(){
+    $codigo = filter_input(INPUT_POST, 'codigo',FILTER_SANITIZE_STRING);
+    $nome = filter_input(INPUT_POST, 'nome',FILTER_SANITIZE_STRING);
+    $cpfp = filter_input(INPUT_POST, 'cpfp',FILTER_SANITIZE_STRING);
+
+    $saida = "DELETE FROM departamento WHERE codigo = $codigo";
+}
+
+function professor(){
+    $cpfp = filter_input(INPUT_POST, 'cpfp',FILTER_SANITIZE_STRING);
+    $nome = filter_input(INPUT_POST, 'nomep',FILTER_SANITIZE_STRING);
+    $depp = filter_input(INPUT_POST, 'depp',FILTER_SANITIZE_STRING);
+
+    $saida ="DELETE FROM professor WHERE cpfp = $codigo";
+}
+
+function aluno(){
+    $matricula = filter_input(INPUT_POST, 'matricula',FILTER_SANITIZE_STRING);
+    $nome = filter_input(INPUT_POST, 'nome',FILTER_SANITIZE_STRING);
+    $cod_curso = filter_input(INPUT_POST, 'cod_curso',FILTER_SANITIZE_STRING);
+    $cod_disciplina = filter_input(INPUT_POST, 'cod_disciplina',FILTER_SANITIZE_STRING);
+
+    $saida ="DELETE FROM aluno WHERE cpfp = $matricula";
+}
+
+function disciplina(){
+    $codigo = filter_input(INPUT_POST, 'codigo',FILTER_SANITIZE_STRING);
+    $nome = filter_input(INPUT_POST, 'nome',FILTER_SANITIZE_STRING);
+    $depp = filter_input(INPUT_POST, 'depp',FILTER_SANITIZE_STRING);
+
+    $saida ="DELETE FROM professor WHERE cpfp = $codigo";
+}
+
+function professor(){
+    $cpfp = filter_input(INPUT_POST, 'cpfp',FILTER_SANITIZE_STRING);
+    $nome = filter_input(INPUT_POST, 'nomep',FILTER_SANITIZE_STRING);
+    $depp = filter_input(INPUT_POST, 'depp',FILTER_SANITIZE_STRING);
+
+    $saida ="DELETE FROM professor WHERE cpfp = $codigo";
 }
 
  ?>
