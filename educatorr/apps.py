e0534c61c6a5f390c796b5f2f3dd251414a23b0b
Samuel Souza <48895781+samuelsouza001@users.noreply.github.com>
2019-11-28 04:04:48 -0300

    new

--- educatorr/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class EducatorrConfig(AppConfig):
-    name = 'educatorr'
--- educatorr/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-def educatorr(request):
-    return render(request, 'Moodle/index.html', {})
-def curso(request):
-    return render(request, 'Moodle/Curso.html', {})
-def professor(request):
-    return render(request, 'Moodle/Professor.html', {})
-
