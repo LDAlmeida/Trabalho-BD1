da1eb8c795efe09102a336250d819f2ad3cf6fbc
LDAlmeida <LDAlmeida@users.noreply.github.com>
2019-11-28 06:19:01 -0300

    tela de login (sem css)

--- Moodle/login.php
+++ Moodle/login.php
@@ -1,32 +1,44 @@
 <?php
 include_once("conexao.php");
 
-function logar(){
-
+function console_log( $data ){
+    echo '<script>';
+    echo 'console.log('. json_encode( $data ) .')';
+    echo '</script>';
+  }
+  
 $tipoUsuario = filter_input(INPUT_POST,'menu',FILTER_SANITIZE_STRING);
 $usuario = filter_input(INPUT_POST,'usuario',FILTER_SANITIZE_STRING);
 $senha = filter_input(INPUT_POST,'senha',FILTER_SANITIZE_STRING);
 
+console_log([$tipoUsuario,$usuario,$senha]);
+
 if ($tipoUsuario == 'Professor') {
-    $saida ="select * from professor where idProfessor = " + $usuario;
+    $saida ='select "idProfessor",senha from login where "idProfessor" = ' . $usuario . ' and senha=' . "'".$senha."'";
     try {
         $resultado = pg_query($conn, $saida);
-        if ($resultado== TRUE) {
-            echo header('Location: ' . $_SERVER['HTTP_REFERER']);
+        console_log($resultado);
+        if (pg_affected_rows($resultado) == 1) {
+            header('Location: Notas.html');
+        }else{
+            header('Location: login.html');
         }
     } catch (\Throwable $th) {
-        //throw $th;
+        throw $th;
     }
 }elseif ($tipoUsuario == 'Aluno') {
-        $saida ="select * from professor where idAluno = " + $usuario;
+        $saida ='select "idAluno",senha from login where "idAluno" = ' . $usuario . ' and senha=' . "'".$senha."'";
         try {
             $resultado = pg_query($conn, $saida);
-            if ($resultado== TRUE) {
-                echo header('Location: ' . $_SERVER['HTTP_REFERER']);
+            console_log($resultado);
+            if (pg_affected_rows($resultado) == 1) {
+                header('Location: Matriculadas.html');
+            }else{
+                header('Location: login.html');
             }
         } catch (\Throwable $th) {
-            //throw $th;
+            throw $th;
         }
     }
-} 
+
 ?>
