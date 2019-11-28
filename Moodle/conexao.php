c9a5ed897b1723b2a6afac39a28355c21d5ee104
JoaoZulato <56926650+JoaoZulato@users.noreply.github.com>
2019-11-28 01:47:34 -0300

    Add files via upload

--- Moodle/conexao.php
+++ Moodle/conexao.php
@@ -1,5 +1,11 @@
 <?php
-$bdcon = pg_connect("host=educatorrdb.ccqy8084hset.sa-east-1.rds.amazonaws.com port=5432 dbname=postgres user=postgres password=postgres");
+$servidor = "localhost";
+$usuario = "root";
+$senha = "";
+$dbname = "BD";
+
+
+$conn = mysqli_connect($servidor,$usuario,$senha,$dbname);
 
 
 ?>
