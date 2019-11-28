c81c417210e75901ce7cf4713f75a0d2d1711f63
JoaoZulato <56926650+JoaoZulato@users.noreply.github.com>
2019-11-28 19:21:03 -0300

    Add files via upload

--- Moodle/deletar.php
+++ Moodle/deletar.php
@@ -1,18 +1,7 @@
 <?php
-
-if(isset($_POST["professor"])){
-       
-    professor();
-}
-elseif(isset($_POST["aluno"])) {
-    aluno(); 
-}
-elseif(isset($_POST["disciplina"])) {
-    disciplina(); 
-}
+include_once("conexao.php");
 
 function professor(){
-    include_once("conexao.php");
     $cpfp = filter_input(INPUT_POST, 'cpfp',FILTER_SANITIZE_STRING);
     $nome = filter_input(INPUT_POST, 'nomep',FILTER_SANITIZE_STRING);
     $depp = filter_input(INPUT_POST, 'depp',FILTER_SANITIZE_STRING);
@@ -21,7 +10,6 @@ function professor(){
 }
 
 function aluno(){
-    include_once("conexao.php");
     $matricula = filter_input(INPUT_POST, 'matricula',FILTER_SANITIZE_STRING);
     $nome = filter_input(INPUT_POST, 'nome',FILTER_SANITIZE_STRING);
     $cod_curso = filter_input(INPUT_POST, 'cod_curso',FILTER_SANITIZE_STRING);
@@ -31,11 +19,10 @@ function aluno(){
 }
 
 function disciplina(){
-    include_once("conexao.php");
     $codigo = filter_input(INPUT_POST, 'codigo',FILTER_SANITIZE_STRING);
     $nome = filter_input(INPUT_POST, 'nome',FILTER_SANITIZE_STRING);
     $depp = filter_input(INPUT_POST, 'depp',FILTER_SANITIZE_STRING);
 
-    $saida ="DELETE FROM disciplina WHERE cod_disciplina = $codigo";
+    $saida ="DELETE FROM professor WHERE cpfp = $codigo";
 }
 
--- Moodle/inserir.php
+++ Moodle/inserir.php
@@ -1,50 +1,58 @@
 <?php
+var_dump($_POST);
 
-
-    if(isset($_POST["professor"])){
-       
+    if($_POST['id'] ==1){
+        
         professor();
     }
-    elseif(isset($_POST["departamento"])) {
+    else
+    if($_POST['id']==2) {
+      
        departamento(); 
     }
-    elseif(isset($_POST["aluno"])) {
+    else
+    if($_POST['id']==3) {
+       
         aluno(); 
-    }
-    elseif(isset($_POST["disciplina"])) {
+     }
+    else
+     if($_POST['id']==4) {
         disciplina(); 
-    }
-    elseif(isset($_POST["curso"])) {
+     }
+    else
+     if($_POST['id']==5) {
         curso(); 
-    }
+     }
 
 function departamento(){
     include_once("conexao.php");
-    $codigo = filter_input(INPUT_POST, 'código',FILTER_SANITIZE_STRING);
     $nome = filter_input(INPUT_POST, 'nome',FILTER_SANITIZE_STRING);
+    $codigo = filter_input(INPUT_POST, 'código',FILTER_SANITIZE_STRING);
+   
     $dnr = filter_input(INPUT_POST, 'dnr',FILTER_SANITIZE_STRING);
 
-    $saida ="INSERT INTO departamento(código,nome,dnr) VALUES ($codigo,$nome,$dnr)";
+    $saida ="INSERT INTO departamento(código,nome,dnr) VALUES ($codigo,'$nome',$dnr)";
     $resultado = pg_query($conn, $saida);
 }
 
 function aluno(){
+    echo 'ola';
     include_once("conexao.php");
-    $matricula = filter_input(INPUT_POST, 'matrícula',FILTER_SANITIZE_STRING);
-    $nome = filter_input(INPUT_POST, 'nome',FILTER_SANITIZE_STRING);
-    $cod_curso = filter_input(INPUT_POST, 'cod_curso',FILTER_SANITIZE_STRING);
+    $matricula = filter_input(INPUT_POST, 'matricula',FILTER_SANITIZE_STRING);
+    $nome = filter_input(INPUT_POST, 'nomea',FILTER_SANITIZE_STRING);
+    $cod_curso = filter_input(INPUT_POST, 'curso',FILTER_SANITIZE_STRING);
 
-    $saida ="INSERT INTO aluno(matrícula,nome,cod_curso) VALUES ($matricula,$nome,$cod_curso)";
+    $saida ="INSERT INTO aluno(matrícula,nome,cod_curso) VALUES ($matricula,'$nome',$cod_curso)";
     $resultado = pg_query($conn, $saida);
 }
 
 function disciplina(){
     include_once("conexao.php");
-    $cod_disciplina = filter_input(INPUT_POST, 'cod_disciplina',FILTER_SANITIZE_STRING);
-    $nome = filter_input(INPUT_POST, 'nome',FILTER_SANITIZE_STRING);
+    $cod_disciplina = filter_input(INPUT_POST, 'codigo',FILTER_SANITIZE_STRING);
+    $nome = filter_input(INPUT_POST, 'nomed',FILTER_SANITIZE_STRING);
     $dnr = filter_input(INPUT_POST, 'dnr',FILTER_SANITIZE_STRING);
 
-    $saida ="INSERT INTO disciplina(cod_disciplina,nome,dnr) VALUES ($cod_disciplina,$nome,$dnr)";
+    $saida ="INSERT INTO disciplina(cod_disciplina,nome,dnr) VALUES ($cod_disciplina,'$nome',$dnr)";
     $resultado = pg_query($conn, $saida);
 }
 
@@ -59,14 +67,17 @@ function curso(){
 }
 
 
+
 function professor (){
+   
     include_once("conexao.php");
-    $cpfp = filter_input(INPUT_POST, 'cpfp',FILTER_SANITIZE_STRING);
     $nomep = filter_input(INPUT_POST, 'nomep',FILTER_SANITIZE_STRING);
+    $cpfp = filter_input(INPUT_POST, 'cpfp',FILTER_SANITIZE_STRING);
+    
     $depp = filter_input(INPUT_POST, 'depp',FILTER_SANITIZE_STRING);
 
     $saida ="INSERT INTO professor(cpf,nome,dnúmero) VALUES ($cpfp,'$nomep',$depp)";
-
+  
     $resultado = pg_query($conn, $saida);
 
 }
